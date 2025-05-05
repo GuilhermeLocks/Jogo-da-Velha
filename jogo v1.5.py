64e22f92bcafd5ea88778b6b8f9a75fcc0bbed02
Guilherme da Silva Locks <guisilvalocks@gmail.com>
2025-05-05 10:55:31 -0300

    jogo v1.5

--- jogo v1.5.py
+++ jogo v1.5.py
@@ -3,8 +3,7 @@ lista_1 = [1,1,1,1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,5,5,6,7,7,8]
 lista_2 = [2,3,4,5,7,9,3,5,8,5,6,7,9,5,6,7,6,7,8,9,9,8,9,9]
 lista_3 = [3,2,7,9,4,5,1,8,5,7,9,5,6,6,5,1,4,3,2,1,3,9,8,7]
 jogadas_diponiveis = ['x', 1, 2, 3, 4, 5, 6, 7, 8, 9]
-jogos = jogo_1 = jogada_computador = jogada = 0
-result = ''
+jogos = jogo_1 = jogada_computador = jogada = result = 0
 ########################################## REALIZA A JOGADA DO JOGADOR ############################
 def velha(jogada):
     for c in range(0, 10):
@@ -26,9 +25,9 @@ def velha(jogada):
 '''.format(jogadas_diponiveis[1], jogadas_diponiveis[2], jogadas_diponiveis[3], jogadas_diponiveis[4],
 jogadas_diponiveis[5], jogadas_diponiveis[6] ,jogadas_diponiveis[7] ,jogadas_diponiveis[8], jogadas_diponiveis[9]))
 ########################################## O JOGO #################################################
-print(velha)
+velha(jogada)
 ########################################## JOGADA #################################################
-while result == '':
+while result == 0:
     while True:
         jogada = input('Qual sua jogada? ')
         if jogada.isnumeric() == True:
@@ -50,9 +49,11 @@ while result == '':
             jogos += 1
             break
 ####################################### JOGADA PARA O COMPUTADOR NÃO PERDER ###################################
-        elif jogadas_diponiveis[lista_1[c]] == jogadas_diponiveis[lista_2[c]] and jogadas_diponiveis[lista_3[c]] != 'o' and jogadas_diponiveis[lista_3[c]] != 'x':
+        elif jogadas_diponiveis[lista_1[c]] == jogadas_diponiveis[lista_2[c]] and jogadas_diponiveis[lista_3[c]] != 'o' and jogadas_diponiveis[lista_3[c]] != 'x' and jogos == 0:
+            jogo_1 = jogadas_diponiveis[lista_3[c]]
             jogadas_diponiveis[lista_3[c]] = 'o'
             jogos += 1
+            print('Computador jogou: {}'.format(jogo_1))
 ######################################### PRIMEIRA JOGADA COMPUTADOR ######################################
     if jogos == 0:
         if jogada_computador == 0:
@@ -78,18 +79,19 @@ while result == '':
                     jogadas_diponiveis[5] = 'o'
                     jogo_1 = 5
                     jogada_computador += 1
-        else:
+            else:
 ######################################### JOGADA ALEATÓRIA COMPUTADOR ######################################
-            while True:
-                jogada_computador = jogadas_diponiveis[random.randint(1, (len(jogadas_diponiveis)) - 1)]
-                if jogada_computador != 'o' and jogada_computador != 'x':
-                    break
-            for c in range(1, 10):
-                if jogada_computador == c:
-                    jogadas_diponiveis[c] = 'o'
-                    jogo_1 = c
+                while True:
+                    jogada_computador = jogadas_diponiveis[random.randint(1, (len(jogadas_diponiveis)) - 1)]
+                    if jogada_computador != 'o' and jogada_computador != 'x':
+                        break
+                for c in range(1, 10):
+                    if jogada_computador == c:
+                        jogadas_diponiveis[c] = 'o'
+                        jogo_1 = c
         jogos = 0
         print('Computador jogou: {}'.format(int(jogo_1)))
+    velha(jogada)
 ########################################## O JOGO #################################################
 velha(jogada)
 ################################################ FIM DO JOGO #####################################################
