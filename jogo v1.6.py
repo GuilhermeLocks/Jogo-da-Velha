6671ae82f3cae1013c225e4fefd2054221658c0f
Guilherme da Silva Locks <guisilvalocks@gmail.com>
2025-05-06 20:03:21 -0300

    jogo v1.6

--- jogo v1.6.py
+++ jogo v1.6.py
@@ -1,1 +1,90 @@
-print
+import random   # DADOS E BIBLIOTECA PARA O JOGO
+lista_1 = [1,1,1,1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,5,5,6,7,7,8]
+lista_2 = [2,3,4,5,7,9,3,5,8,5,6,7,9,5,6,7,6,7,8,9,9,8,9,9]
+lista_3 = [3,2,7,9,4,5,1,8,5,7,9,5,6,6,5,1,4,3,2,1,3,9,8,7]
+jogos = jogada_computador = jogada = result = 0
+jogadas_diponiveis = ['x', 1, 2, 3, 4, 5, 6, 7, 8, 9]
+def velha(jogada):   # REALIZA A JOGADA DO JOGADOR
+    for c in range(0, 10):
+        if jogada == c:
+            jogadas_diponiveis[c] = 'x'
+    print('''
+------------------------------
+         JOGO DA VELHA          
+------------------------------
+
+        {}  |  {}  |  {}
+       ------------------
+        {}  |  {}  |  {}
+       ------------------
+        {}  |  {}  |  {}
+
+------------------------------
+'''.format(jogadas_diponiveis[1], jogadas_diponiveis[2], jogadas_diponiveis[3], jogadas_diponiveis[4],
+jogadas_diponiveis[5], jogadas_diponiveis[6] ,jogadas_diponiveis[7] ,jogadas_diponiveis[8], jogadas_diponiveis[9]))
+velha(jogada)
+while result == 0:
+    while True: # JOGADA
+        jogada = input('Qual sua jogada? ')
+        if jogada.isnumeric() == True:
+            jogada = int(jogada)
+            if jogada in jogadas_diponiveis:
+                jogadas_diponiveis.remove(jogada)
+                jogadas_diponiveis.insert(jogada, 'x')
+                break
+            else:
+                print('Essa posição ja esta oculpada, tente novamente')
+        else:
+            print('Jogada invalida tente novamente')
+    for c in range(0, 24):  # JOGADA PARA O COMPUTADOR GANHAR
+        if jogadas_diponiveis[lista_1[c]] == jogadas_diponiveis[lista_2[c]] == 'o' and jogadas_diponiveis[lista_3[c]] != 'o' and jogadas_diponiveis[lista_3[c]] != 'x' and jogos == 0:
+            jogadas_diponiveis[lista_3[c]] = 'o'
+            result = 'o'
+            jogos += 1
+            break
+    for c in range(0, 24):  # JOGADA PARA O COMPUTADOR NÃO PERDER
+        if jogadas_diponiveis[lista_1[c]] == jogadas_diponiveis[lista_2[c]] == 'x' and jogadas_diponiveis[lista_3[c]] != 'o' and jogadas_diponiveis[lista_3[c]] != 'x' and jogos == 0:
+            jogada_computador = jogadas_diponiveis[lista_3[c]]
+            jogadas_diponiveis[lista_3[c]] = 'o'
+            jogos += 1
+            print('Computador jogou: {}'.format(jogada_computador))
+    if jogos == 0:  # PRIMEIRA JOGADA COMPUTADOR
+        if jogadas_diponiveis[5] == 'x':
+            if jogadas_diponiveis[3] == 3:
+                jogadas_diponiveis[3] = 'o'
+                jogada_computador = 3
+                jogos += 1
+            elif jogadas_diponiveis[1] == 1:
+                jogadas_diponiveis[1] = 'o'
+                jogada_computador = 1
+                jogos += 1
+            elif jogadas_diponiveis[7] == 7:
+                jogadas_diponiveis[7] = 'o'
+                jogada_computador = 7
+                jogos += 1
+            elif jogadas_diponiveis[9] == 9:
+                jogadas_diponiveis[9] = 'o'
+                jogada_computador = 9
+                jogos += 1
+        elif jogadas_diponiveis[1] == 'x' or jogadas_diponiveis[3] == 'x' or jogadas_diponiveis[7] == 'x' or jogadas_diponiveis[9] == 'o':
+            if jogadas_diponiveis[5] == 5:
+                jogadas_diponiveis[5] = 'o'
+                jogada_computador = 5
+                jogos += 1
+        if jogos == 0:  # JOGADA ALEATÓRIA COMPUTADOR
+            while True:
+                jogada_computador = jogadas_diponiveis[random.randint(1, (len(jogadas_diponiveis)) - 1)]
+                if jogada_computador != 'o' and jogada_computador != 'x':
+                    break
+            for c in range(1, 10):
+                if jogada_computador == c:
+                    jogadas_diponiveis[c] = 'o'
+                    jogada_computador = c
+    print('Computador jogou: {}'.format(int(jogada_computador)))
+    velha(jogada)
+    jogos = 0
+velha(jogada)
+if result == 'x':
+    print('jogo ganho')
+elif result == 'o':
+    print('jogo perdido')
